9be5fc6c9429fe1c123f6351e10ff74924403c03
RobertoJudet <roberto.judet@itschool.ro>
2023-03-17 19:58:38 +0200

    Add homework1

--- finance-project/api/users.py
+++ finance-project/api/users.py
@@ -2,6 +2,12 @@ from fastapi import APIRouter
 
 users_router = APIRouter(prefix="/users")
 
+# Homework 1 for Project
+# implement get, create and delete user in domain too (user repo & user factory)
+# also create api models
+# create tests for repo & factory
+# username should be at least 6 chars and max 20 chars, it can only contain letter, numbers & -
+# save the user list in a file
 
 @users_router.get("/")
 def get_all_users():

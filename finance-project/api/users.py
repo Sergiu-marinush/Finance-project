6aac792cd1c1548c0417d1772bf88e995814fed9
RobertoJudet <roberto.judet@itschool.ro>
2023-03-24 19:38:03 +0200

    Add homework

--- finance-project/api/users.py
+++ finance-project/api/users.py
@@ -19,6 +19,15 @@ repo = UserRepo("main_users.json")
 def get_all_users():
     return repo.get_all()
 
+# TODO homework, replace username with an id
+# when we create a user we should create a uuid for it
+# when we return all the user, each user should have the id field
+# when we query a single user or delete a user we should pass the id
+
+# create POST /<user_id>/stocks
+# the user can add a stock to its portfolio, by giving the ticker and the number of units it has
+# save the country, full name of the company
+# when we get a specific user we get the price of every stock the user has and the money it has on it
 
 @users_router.get("/{username}", response_model=UserInfo)
 def get_user(username: str):

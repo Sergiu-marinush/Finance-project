7e7350de8326f80f4b1c3d13df5622931b029353
RobertoJudet <roberto.judet@itschool.ro>
2023-03-30 21:06:27 +0300

    Add course35

--- finance-project/main.py
+++ finance-project/main.py
@@ -3,10 +3,16 @@ from api.users import users_router
 from api.assets import assets_router
 from domain.user.factory import InvalidUsername
 from starlette.responses import JSONResponse
+from logging import getLogger, FileHandler
+
+logger = getLogger("financeLogger")
+handler = FileHandler("finance.log")
+logger.addHandler(handler)
 
 app = FastAPI(
     debug=True,
     title="Fintech Portfolio API",
+    # TODO add to README
     description="A webserver with a REST API for keeping track of your different financial assets,"
     " stocks & crypto, and see/compare their evolution",
     version="0.3.0",
@@ -14,11 +20,19 @@ app = FastAPI(
 app.include_router(users_router)
 app.include_router(assets_router)
 
+
 @app.exception_handler(InvalidUsername)
 def return_invalid_username(_: Request, e: InvalidUsername):
-    return JSONResponse(status_code=400, content="Username is not valid! Error: " + str(e))
+    return JSONResponse(
+        status_code=400, content="Username is not valid! Error: " + str(e)
+    )
+
 
 if __name__ == "__main__":
     import subprocess
 
-    subprocess.run(["uvicorn", "main:app", "--reload"])
+    logger.info("Starting webserver ...")
+    try:
+        subprocess.run(["uvicorn", "main:app", "--reload"])
+    except Exception as e:
+        logger.warning("Webserver has stopped. Reason: " + str(e))

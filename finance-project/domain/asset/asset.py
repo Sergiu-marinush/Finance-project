cf33f4adbaa5a072a6da157938d72b3a55fa8be1
sergi <sergiu.marinush@yahoo.com>
2023-04-04 17:38:19 +0100

    Refactor query in asset class

--- finance-project/domain/asset/asset.py
+++ finance-project/domain/asset/asset.py
@@ -1,3 +1,6 @@
+import yfinance
+
+
 class Asset:
     def __init__(self, ticker: str, nr: float, name: str, country: str, sector: str):
         self.__ticker = ticker
@@ -5,3 +8,49 @@ class Asset:
         self.__name = name
         self.__country = country
         self.__sector = sector
+        yfin = yfinance.Ticker(ticker)
+        self.__info = yfin.fast_info
+
+    @property
+    def ticker(self) -> str:
+        return self.__ticker
+
+    @property
+    def units(self) -> float:
+        return self.__nr
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @property
+    def country(self) -> str:
+        return self.__country
+
+    @property
+    def current_price(self) -> float:
+        # lazy - loading
+        # dict
+        # with keys =['currency', 'dayHigh', 'dayLow', 'exchange',
+        # 'fiftyDayAverage', 'lastPrice', 'lastVolume', 'marketCap',
+        # 'open', 'previousClose', 'quoteType', 'regularMarketPreviousClose',
+        # 'shares', 'tenDayAverageVolume', 'threeMonthAverageVolume', 'timezone',
+        # 'twoHundredDayAverage', 'yearChange', 'yearHigh', 'yearLow']
+
+        price = self.__info["lastPrice"]
+        return round(price, 2)
+
+    @property
+    def currency(self) -> str:
+        return self.__info["currency"]
+
+    @property
+    def closed_price(self) -> float:
+        return self.__info["previousClose"]
+
+    @property
+    def fifty_day_price(self) -> float:
+        return self.__info["fiftyDayAverage"]
+
+    #TODO a property, in percentage how much it went up or down
+    # current_price & closed_price
